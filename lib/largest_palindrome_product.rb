d55734b852d3780d5e4c9ff8eac2cea2571fb3da
Matthew Williams <matthew.williams@flatironschool.com>
2021-06-27 17:35:39 -0600

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,58 @@
 # Implement your procedural solution here!
+class LargestPalindromeProduct
+
+    def answer
+        num1 = 999
+        num2 = 999
+        prod = num1 * num2
+        
+        palindrome_target = findNextPalindrome(prod)
+        
+        while palindrome_target % num1 != 0
+            num1 -= 1
+            if num1 < 100 || palindrome_target / num1 > 999
+                nextPalindrome = findNextPalindrome(palindrome_target)
+                palindrome_target = nextPalindrome
+                num1 = 999
+            end 
+        end 
+           palindrome_target
+        
+        end 
+        
+        def findNextPalindrome(num)
+            if(palindrome(num))
+                num -= 1
+            end
+            while !palindrome(num)
+                num -= 1
+            end 
+            num
+        end 
+        
+        
+        
+        def palindrome(num)
+        
+            string_num = num.to_s
+        
+            if(string_num.length == 1)
+                return false
+            elsif string_num.length ==2
+                if(string_num[0] == string_num[string_num.length-1])
+                    return true
+                else 
+                    return false
+                end  
+            else
+                if string_num[0] == string_num[string_num.length-1]
+                    palindrome(string_num.slice(1, string_num.length-2))
+                else
+                    return false
+                end 
+            end 
+        
+        
+        end 
+
+end 
--- spec/largest_palindrome_product_spec.rb
+++ spec/largest_palindrome_product_spec.rb
@@ -1,6 +1,7 @@
 describe "#largest_palindrome_product" do
   it 'returns the largest palindrome product of two 3-digit numbers' do
     answer = largest_palindrome_product
+    puts answer
     expect(encode_answer(answer)).to eq("55d264f8671b50b5dbaffa56d1ee719fd429e8f4")
   end
 end
